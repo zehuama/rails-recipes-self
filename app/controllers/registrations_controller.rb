be6b51aa8fd2a0a4976cb8e03bfe67c79c15af91
javiel <zehua046@gmail.com>
2019-05-28 11:46:10 +0800

    finish all 百宝箱“

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < ApplicationController
   before_action :find_event
+  before_action :set_pending_registration, :only => [:step1, :step1_update, :step2, :step2_update, :step3, :step3_update]
 
   def new
 
@@ -13,6 +14,7 @@ class RegistrationsController < ApplicationController
     @registration.current_step = 1
 
     if @registration.save
+      CheckRegistrationJob.set(wait: 1.minute).perform_later(@registration.id)
       redirect_to step2_event_registration_path(@event, @registration)
     else
       flash.now[:alert] = @registration.errors[:base].join("、")
@@ -25,11 +27,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step1
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step1_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.current_step = 1
 
     if @registration.update(registration_params)
@@ -40,11 +40,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step2
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step2_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.current_step = 2
 
     if @registration.update(registration_params)
@@ -55,11 +53,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step3
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step3_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.status = "confirmed"
     @registration.current_step = 3
 
@@ -86,6 +82,15 @@ class RegistrationsController < ApplicationController
   end
 
   protected
+
+  def set_pending_registration
+    @registration = @event.registrations.find_by_uuid(params[:id])
+
+    if @registration.status == "cancalled"
+      flash[:alert] = "请重新报名"
+      redirect_to event_path(@event)
+    end
+  end
 
   def registration_params
     params.require(:registration).permit(:ticket_id, :name, :email, :cellphone, :website, :bio)
